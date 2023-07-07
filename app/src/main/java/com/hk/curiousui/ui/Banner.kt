28cb9c29fa830246b94791361727134b54965be6
6879756e <6879756e@gmail.com>
2023-07-07 20:23:18 +0900

    Implement Banner indicator
    The indicator lets you know which item the banner is currently showing

--- app/src/main/java/com/hk/curiousui/ui/Banner.kt
+++ app/src/main/java/com/hk/curiousui/ui/Banner.kt
@@ -1,16 +1,30 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.hk.curiousui.ui
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,13 +38,13 @@ import kotlinx.coroutines.delay
  * TODO:
 DONE * 1. Make it take in a list of drawable resources, or provide a composable for a given int? (Int) -> @Composable
 DONE * 2. Make it automatically slide every couple of seconds (scrolling from last page to first page has undesirable animation (backward scroll). Need to work on this!)
- * 3. Add circles beneath which indicate the current ad position
- * 4. Make circles clickable which causes the ad to swipe to that position
+DONE * 3. Add circles beneath which indicate the current ad position
+REJECTED * 4. Make circles clickable which causes the ad to swipe to that position
  * 5. When user is pressing on item, pause number 2.
+ * 6. Make 2 types of indicators, circular and text, but position them differently.
  */
 
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun FoundationBanner(
     drawableResources: List<Int>,
@@ -48,11 +62,12 @@ fun FoundationBanner(
 
     Card(
         modifier = Modifier
-            .width(bannerWidth)
-            .height(bannerHeight)
             .padding(horizontal = 24.dp)
     ) {
         HorizontalPager(
+            modifier = Modifier
+                .width(bannerWidth)
+                .height(bannerHeight),
             state = pagerState,
             pageCount = drawableResources.size,
             beyondBoundsPageCount = 1,
@@ -63,10 +78,47 @@ fun FoundationBanner(
                 contentScale = ContentScale.Crop
             )
         }
+
+        BannerIndicator(pagerState.currentPage, drawableResources.size)
+    }
+}
+
+@Composable
+private fun BannerIndicator(
+    currentIndex: Int,
+    itemCount: Int,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        repeat(itemCount) {
+            val indicator = Modifier
+                .size(8.dp)
+                .clip(CircleShape)
+                .border(1.dp, Color.Black, CircleShape)
+
+            if (it == currentIndex) {
+                Box(modifier = indicator.background(Color.Black))
+            } else {
+                Box(modifier = indicator)
+            }
+
+            Spacer(modifier = Modifier.width(4.dp))
+        }
     }
 }
 
 @Preview
+@Composable
+fun BannerIndicatorPreview() {
+    BannerIndicator(3, 10)
+}
+
+@Preview(name = "Light mode")
+@Preview(name = "Dark mode", uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun FoundationBannerPreview() {
     val drawableResources = listOf(
