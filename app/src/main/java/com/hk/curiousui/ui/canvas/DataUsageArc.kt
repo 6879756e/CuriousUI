9d41a11c170e90164b7e546caa93d68107fbeb6c
6879756e <6879756e@gmail.com>
2023-07-11 13:01:48 +0900

    Implement extract ProportionArc from DataUsageArc and refactor
    Made DataUsageArc much more intuitive to use by no longer taking up random space above and below the arc - instead, it fills up the entire space provided.

--- app/src/main/java/com/hk/curiousui/ui/canvas/DataUsageArc.kt
+++ app/src/main/java/com/hk/curiousui/ui/canvas/DataUsageArc.kt
@@ -1,9 +1,13 @@
 package com.hk.curiousui.ui.canvas
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -11,29 +15,55 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathMeasure
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
 
 
-@Preview
 @Composable
 fun DataUsageArc(
-    remainingProportion: Float = 0.8f,
-    strokeWidth: Float = 30.dp.value,
+    remainingProportion: Float,
+    totalData: Float,
 ) {
-    Canvas(
-        modifier = Modifier
-            .fillMaxWidth()
-            .aspectRatio(1f)
+    Column(
+        modifier = Modifier.size(200.dp, 100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val h = drawContext.size.height
-        val w = drawContext.size.width
-        val y = h / 3
+        ProportionArc(
+            remainingProportion = remainingProportion,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        )
+
+        val leftoverData = String.format("%.2f", remainingProportion * totalData)
+
+        Text("Data remaining", style = MaterialTheme.typography.titleLarge)
+        Text(
+            "${leftoverData}GB",
+            style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+        )
+    }
+}
+
+@Composable
+fun ProportionArc(
+    remainingProportion: Float,
+    modifier: Modifier,
+    strokeWidth: Float = 30.dp.value
+) {
+    Canvas(modifier = modifier) {
+        val height = this.size.height
+        val width = this.size.width
 
         val fullPath = Path().apply {
-            moveTo(strokeWidth / 2, y)
+            moveTo(strokeWidth / 2, height - strokeWidth / 2)
             quadraticBezierTo(
-                w / 2, 0f, w - strokeWidth / 2, y
+                width / 2,
+                -height + (strokeWidth * 1.5).roundToInt(),
+                width - strokeWidth / 2,
+                height - strokeWidth / 2
             )
         }
 
@@ -57,12 +87,24 @@ fun DataUsageArc(
 
         drawPath(
             brush = Brush.linearGradient(
-                0.0f to Color(0xFF3976c4),
-                0.4f to Color(0xFF63c4c1)
+                0.0f to Color(0xFF3976C4),
+                0.4f to Color(0xFF63C4C1)
             ),
             path = remainingPath,
             style = Stroke(width = strokeWidth, cap = StrokeCap.Round)
         )
     }
 
+}
+
+@Preview
+@Composable
+fun DataUsageArcPreview() {
+    DataUsageArc(0.4f, 10f)
+}
+
+@Preview
+@Composable
+fun ProportionArcPreview() {
+    ProportionArc(0.8f, modifier = Modifier.size(300.dp, 200.dp))
 }
